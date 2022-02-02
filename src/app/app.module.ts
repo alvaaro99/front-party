c5bdf573744ec2fd5bbbdb90efe7b83a752b995b
Alvaro Acosta <alvaro99acosta@gmail.com>
2022-02-02 21:09:29 +0100

    import sockets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,25 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { SocketIoModule } from 'ngx-socket-io';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatSocket } from './sockets/chat.socket';
+import { SongsSocket } from './sockets/songs.socket';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule,HttpClientModule, TranslateModule.forRoot({
+  imports: [BrowserModule,HttpClientModule,SocketIoModule , TranslateModule.forRoot({
     loader: {
       provide: TranslateLoader,
       useFactory: HttpLoaderFactory,
       deps: [HttpClient]
     }
   }), IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ChatSocket, SongsSocket],
   bootstrap: [AppComponent],
 })
 export class AppModule {
