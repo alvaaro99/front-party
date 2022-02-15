a9894c8b9ed6e67b7fd4bf6a3f224d3c97ed71b0
Alvaro Acosta <alvaro99acosta@gmail.com>
2022-02-15 18:18:24 +0100

    check if token is valid

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private storage: Storage) {}
+  constructor(private storage: Storage, private http: HttpClient) {}
 
   async isUserLogged(): Promise<boolean> {
     const token = await this.storage.get('access_token');
-    if (token) {
+    if (token && (await this.validateToken(token))) {
       return true;
     }
     return false;
+  }
+
+  async validateToken(token) {
+    return this.http
+      .get(`${environment.backUrl}/auth/logged`, {
+        headers: { Authorization: token },
+      })
+      .toPromise()
+      .then(() => true)
+      .catch(() => false);
   }
 }
