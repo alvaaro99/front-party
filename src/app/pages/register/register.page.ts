192a1103e6611d656dcf82fbc4aff11ac53aefaf
Alvaro Acosta <alvaro99acosta@gmail.com>
2022-02-25 17:15:43 +0100

    register page

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserRegisterDto } from 'src/app/models/userRegister.dto';
+import { ConfirmedValidator } from 'src/app/validators/confirm-password.validator';
+import { RegisterService } from './register.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  constructor(
+    private formBuilder: FormBuilder,
+    private registerService: RegisterService
+  ) {}
 
-  constructor() { }
+  public userToRegister: FormGroup = this.formBuilder.group(
+    {
+      email: ['', [Validators.required, Validators.email]],
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(3)]],
+      repeatPassword: ['', Validators.required],
+    },
+    {
+      validator: ConfirmedValidator('password', 'repeatPassword'),
+    }
+  );
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  register() {
+    const { repeatPassword, ...newUser } = this.userToRegister.value;
+    this.registerService.register(newUser);
   }
-
 }
---
+++ src/app/pages/register/register.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
+import { tap } from 'rxjs/operators';
+import { UserRegisterDto } from 'src/app/models/userRegister.dto';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RegisterService {
+  constructor(
+    private http: HttpClient,
+    private storage: Storage,
+    private router: Router
+  ) {}
+
+  register(user: UserRegisterDto) {
+    this.http
+      .post(`${environment.backUrl}/auth/register`, user, {
+        headers: { 'Content-Type': 'application/json' },
+      })
+      .pipe(
+        tap((response) => {
+          this.storage.set(
+            'access_token',
+            `Bearer ${response['access_token']}`
+          );
+        }),
+        tap(() => {
+          this.router.navigateByUrl('');
+        })
+      )
+      .subscribe();
+  }
+}
