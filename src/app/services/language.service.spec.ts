be8dfa82641d3bf85bcb4e3c7c33a34741e891ea
Alvaro Acosta <alvaro99acosta@gmail.com>
2022-02-25 16:17:28 +0100

    test for language service

--- src/app/services/language.service.spec.ts
+++ src/app/services/language.service.spec.ts
@@ -18,4 +18,13 @@ describe('LanguageService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should be language set ES', async () => {
+    spyOn(service['storage'], 'get')
+      .withArgs(service.LANG_KEY)
+      .and.resolveTo('es');
+    service.setLanguage('es');
+    expect(service.langSelected).toEqual('es');
+    expect(await service['storage'].get(service.LANG_KEY)).toEqual('es');
+  });
 });
