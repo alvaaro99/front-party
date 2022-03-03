84378ad7e7f9158b8f93d91ffbd91aefd322264a
Alvaro Acosta <alvaro99acosta@gmail.com>
2022-03-03 20:12:50 +0100

    logout and changePassword implemented

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 import { User } from '../models/user';
 import { UserService } from '../services/user.service';
 
@@ -8,7 +11,14 @@ import { UserService } from '../services/user.service';
   styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page implements OnInit {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private alertController: AlertController,
+    public translateService: TranslateService
+  ) {}
+
+  user: User = { name: 'NAME', email: 'EMAIL' };
 
   ngOnInit(): void {
     this.userService
@@ -16,5 +26,108 @@ export class Tab3Page implements OnInit {
       .then((obs$) => obs$.subscribe((user: User) => (this.user = user)));
   }
 
-  user: User = { name: 'NAME', email: 'EMAIL' };
+  async showAlertChangePassword() {
+    const alert = await this.alertController.create({
+      header: this.translateService.instant('changePassword'),
+      message: this.translateService.instant('sureChangePassword'),
+      inputs: [
+        {
+          name: 'oldPassword',
+          type: 'password',
+          placeholder: this.translateService.instant('oldPassword'),
+        },
+        {
+          name: 'newPassword',
+          type: 'password',
+          placeholder: this.translateService.instant('newPassword'),
+        },
+        {
+          name: 'confirmNewPassword',
+          type: 'password',
+          placeholder: this.translateService.instant('confirmNewPassword'),
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          id: 'cancel-button',
+        },
+        {
+          text: this.translateService.instant('change'),
+          role: 'OK',
+          id: 'confirm-button',
+          handler: (values) => this.handleChangePassword(values),
+        },
+      ],
+    });
+    await alert.present();
+  }
+
+  async incorrectPassword() {
+    const alert = await this.alertController.create({
+      header: this.translateService.instant('incorrectPassword'),
+      message: this.translateService.instant('incorrectPasswordExplain'),
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  async passwordChanged() {
+    const alert = await this.alertController.create({
+      header: this.translateService.instant('passwordChanged'),
+      message: this.translateService.instant('passwordChangedExplained'),
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  async newPasswordNotEquals() {
+    const alert = await this.alertController.create({
+      header: this.translateService.instant('tryAgain'),
+      message: this.translateService.instant('passwordsNotEquals'),
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  async invalidPassword() {
+    const alert = await this.alertController.create({
+      header: this.translateService.instant('incorrectPassword'),
+      message: this.translateService.instant('passwordRequisites'),
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  async passwordCanBeEquals() {
+    const alert = await this.alertController.create({
+      header: this.translateService.instant('incorrectPassword'),
+      message: this.translateService.instant('oldAndNewPasswordCantBeEquals'),
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  handleChangePassword({ oldPassword, newPassword, confirmNewPassword }) {
+    if (newPassword !== confirmNewPassword) return this.newPasswordNotEquals();
+    if (!this.isValidPassword(newPassword)) return this.invalidPassword();
+    if (oldPassword === newPassword) return this.passwordCanBeEquals();
+    this.userService.changePassword(oldPassword, newPassword).then((obs$) => {
+      obs$.subscribe(
+        () => this.passwordChanged(),
+        () => this.incorrectPassword()
+      );
+    });
+  }
+
+  private isValidPassword(password) {
+    return password.length >= 3;
+  }
+
+  logout() {
+    this.userService.logout().then(() => {
+      this.router.navigateByUrl('/home');
+    });
+  }
 }
